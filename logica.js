799f9d4d80a9944928fba5f19ffc5713c932b367
Enzo <enzo.pazna@gmail.com>
2026-04-12 21:26:04 -0300

    Casa 1204

--- logica.js
+++ logica.js
@@ -1,17 +1,19 @@
+const bg = document.querySelector(".background-image");
+const card = document.querySelector(".conteudo");
+
 async function BuscarClima() {
     const cidade = document.getElementById("cidade").value;
     const url = `https://goweather.xyz/v2/weather/${cidade}`;
-
     const resposta = await fetch(url);
     const dados = await resposta.json();
 
-    if(dados.message === "NOT_FOUND") {
+    if (dados.message === "NOT_FOUND") {
         document.getElementById("resultado").innerHTML = `
             <p>Cidade não encontrada</p>
         `;
         return;
     }
-    // caso de cidade encontrada
+
     document.getElementById("resultado").innerHTML = `
         <h2>Clima em ${cidade}</h2>
         <p>Temperatura: ${dados.temperature}</p>
@@ -26,3 +28,9 @@ document.getElementById("cidade").addEventListener("keypress", function(event) {
     }
 });
 
+document.addEventListener("mousemove", (e) => {
+    const x = (e.clientX / window.innerWidth - 0.5);
+    const y = (e.clientY / window.innerHeight - 0.5);
+
+    bg.style.transform = `translate(${-x * 20}px, ${-y * 20}px)`;
+});
